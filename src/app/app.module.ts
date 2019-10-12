c40484c25a7c3c41a604489f4923fc7c6e8b9820
deejay94 <dahjoyner12@gmail.com>
2019-10-11 21:53:36 -0400

    Install In-memory web API
    - Installed In-memory as mock API
    - Imported in-memory and http modules to appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';  
+import { DataService } from './services/data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +18,9 @@ import { AddSequenceComponent } from './components/add-sequence/add-sequence.com
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    InMemoryWebApiModule.forRoot(DataService),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
